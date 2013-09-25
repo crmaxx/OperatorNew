110b0f0ec449ce7a101957d6c579bbff4cad572b
Maxim Zhukov <crmaxx@gmail.com>
2013-09-26 03:40:33 +0400

    Initial commit

---
+++ new_operator.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require 'erb'
+require 'csv'
+
+csv_file = File.expand_path('../smsjoin.csv', __FILE__)
+erb_file = File.expand_path('../add_service_number.sql.erb', __FILE__)
+sql_file = File.expand_path('../add_service_number.sql', __FILE__)
+
+def save(file, data)
+  File.open(file, 'a') do |f|
+    f.puts data
+    f.puts "\n"
+  end
+end
+
+save(sql_file, 'spool add_service_number.log')
+
+CSV.foreach(csv_file, col_sep: ';') do |row|
+  @name, @description, @snumber, @in_sms_price, @operator_share, @module_name = row
+  save(sql_file, ERB.new(File.read(erb_file)).result(binding))
+end
+
+save(sql_file, 'spool off')
