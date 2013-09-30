ff001c55c2e5554fbd82eff49a091559520c669d
Maxim Zhukov <crmaxx@gmail.com>
2013-10-01 00:31:17 +0400

    Added country_code

--- new_operator.rb
+++ new_operator.rb
@@ -2,6 +2,8 @@
 
 require 'erb'
 require 'csv'
+require 'open-uri'
+require 'nokogiri'
 
 csv_file = File.expand_path('../platinote-countries.csv', __FILE__)
 erb_file = File.expand_path('../add_service_number.sql.erb', __FILE__)
@@ -14,10 +16,15 @@ def save(file, data)
   end
 end
 
+def get_country_code(country)
+  Nokogiri::HTML(open("http://countrycode.org/#{country}")).css('#main_table_blue_2 tr th h1').text.strip.split(' ').last
+end
+
 save(sql_file, 'spool add_service_number.log')
 
 CSV.foreach(csv_file, col_sep: ';') do |row|
-  @country, @name, @description, @snumber, @in_sms_price, @operator_share, @module_name, @basic, @profi, @premium = row
+  @country, @name, @description, @module_name, @snumber, @in_sms_price, @operator_share, @basic, @profi, @premium = row
+  @country_code = get_country_code(@country)
   save(sql_file, ERB.new(File.read(erb_file)).result(binding))
 end
 
